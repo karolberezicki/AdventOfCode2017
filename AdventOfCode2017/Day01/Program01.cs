a9dea2f52134ed7f92c70a6b9dee99cde081fc3a
Karol Berezicki <karolberezicki@gmail.com>
2017-12-01 23:40:17 +0100

    Day01, part one and two

--- AdventOfCode2017/Day01/Program01.cs
+++ AdventOfCode2017/Day01/Program01.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day01
 {
@@ -13,6 +11,23 @@ namespace Day01
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+
+            List<int> sourceInts = source.Select(c => c - '0').ToList();
+
+            int partOne = sourceInts.Where((c, i) =>
+            i == sourceInts.Count - 1 && sourceInts.First() == sourceInts.Last()
+            ||c == sourceInts[i + 1])
+            .Sum();
+
+            int partTwo = sourceInts.Where((c, i) =>
+            i < sourceInts.Count / 2 && c == sourceInts[i + source.Length / 2] ||
+            i > sourceInts.Count / 2 && c == sourceInts[i - source.Length / 2])
+            .Sum();
+
+            Console.WriteLine($"Part one: {partOne}");
+            Console.WriteLine($"Part two: {partTwo}");
+
+            Console.ReadKey();
         }
     }
 }
