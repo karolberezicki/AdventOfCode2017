6255ad2123348ef30eb9f6fac332de1d8f0c2175
Karol Berezicki <karolberezicki@gmail.com>
2017-12-04 06:18:30 +0100

    Day04, Part one and two

--- AdventOfCode2017/Day04/Program04.cs
+++ AdventOfCode2017/Day04/Program04.cs
@@ -13,6 +13,44 @@ namespace Day04
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+            var input = source.Split('\n').ToList();
+
+            var partOne = input.Select(passphrase => passphrase.Split(' ')).Count(word => word.Length == word.Distinct().Count());
+
+            Dictionary<string, bool> dictOfValidPassphrases = input.ToDictionary(c => c, c => true);
+            foreach (string passphrase in input)
+            {
+                var words = passphrase.Split(' ');
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    for (int j = 0; j < words.Length; j++)
+                    {
+                        if (i == j)
+                        {
+                            continue;
+                        }
+
+                        var a = words[i].ToList();
+                        var b = words[j].ToList();
+                        a.Sort();
+                        b.Sort();
+
+                        if (string.Join("", a) == string.Join("", b))
+                        {
+                            dictOfValidPassphrases[passphrase] = false;
+                        }
+
+                    }
+                }
+            }
+
+            var partTwo = dictOfValidPassphrases.Count(c => c.Value);
+
+            Console.WriteLine($"Part one: {partOne}");
+            Console.WriteLine($"Part two: {partTwo}");
+
+            Console.ReadKey();
         }
     }
 }
