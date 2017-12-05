e8193f9e8cbf1656cc56586f835cf036672725db
Karol Berezicki <karolberezicki@gmail.com>
2017-12-05 06:44:35 +0100

    Day05, Part one and two

--- AdventOfCode2017/Day05/Program05.cs
+++ AdventOfCode2017/Day05/Program05.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day05
 {
@@ -13,6 +11,63 @@ namespace Day05
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+            List<int> input = source.Split('\n').Select(int.Parse).ToList();
+
+            int ip = 0;
+            int steps = 0;
+
+            while (ip < input.Count)
+            {
+                if (ip < 0)
+                {
+                    ip = 0;
+                }
+
+                int c = input[ip];
+
+                steps++;
+
+                input[ip] = input[ip] + 1;
+                ip += c;
+            }
+
+            int partOne = steps;
+
+            input = source.Split('\n').Select(int.Parse).ToList();
+            ip = 0;
+            steps = 0;
+
+            while (ip < input.Count)
+            {
+                if (ip < 0)
+                {
+                    ip = 0;
+                }
+
+                int c = input[ip];
+
+                steps++;
+
+                if (c >= 3)
+                {
+                input[ip] = input[ip] - 1;
+
+                }
+                else
+                {
+                input[ip] = input[ip] + 1;
+
+                }
+                ip += c;
+            }
+
+            int partTwo = steps;
+
+
+            Console.WriteLine($"Part one: {partOne}");
+            Console.WriteLine($"Part two: {partTwo}");
+
+            Console.ReadKey();
         }
     }
 }
