51d07aaf53af253f9c26565f9ab0c97066ec32f1
Karol Berezicki <karolberezicki@gmail.com>
2017-12-06 06:26:50 +0100

    Day06, Part one and two

--- AdventOfCode2017/Day06/Program06.cs
+++ AdventOfCode2017/Day06/Program06.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day06
 {
@@ -13,6 +11,46 @@ namespace Day06
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+            List<int> input = source.Split('\t').Select(int.Parse).ToList();
+            //List<int> input = new List<int>() { 0, 2, 7, 0 }; // example
+
+            HashSet<string> seen = new HashSet<string>();
+
+            int partOne = 0;
+            int partTwo;
+
+            while (true)
+            {
+                string current = string.Join(" ", input);
+
+                if (seen.Contains(current))
+                {
+                    partTwo = partOne - seen.ToList().IndexOf(current);
+                    break;
+                }
+
+                seen.Add(current);
+
+                int maxBlock = input.Max();
+                int indexOfMaxBlock = input.IndexOf(maxBlock);
+
+                input[indexOfMaxBlock] = 0;
+                int index = indexOfMaxBlock;
+                while (maxBlock > 0)
+                {
+                    index = index == input.Count - 1 ? 0 : index + 1;
+                    input[index] = input[index] + 1;
+                    maxBlock--;
+                    indexOfMaxBlock++;
+                }
+
+                partOne++;
+            }
+
+            Console.WriteLine($"Part one: {partOne}");
+            Console.WriteLine($"Part two: {partTwo}");
+
+            Console.ReadKey();
         }
     }
 }
