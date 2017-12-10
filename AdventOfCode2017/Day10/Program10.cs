0d5d0a38afc8e241b1cba223e3b500771709ec36
Karol Berezicki <karolberezicki@gmail.com>
2017-12-10 10:48:51 +0100

    Day10, Part one

--- AdventOfCode2017/Day10/Program10.cs
+++ AdventOfCode2017/Day10/Program10.cs
@@ -13,6 +13,46 @@ namespace Day10
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+            List<int> input = source.Split(',').Select(int.Parse).ToList();
+            int partOne = PartOne(input);
+        }
+
+        private static int PartOne(IEnumerable<int> input)
+        {
+            List<int> list = Enumerable.Range(0, 256).ToList();
+
+            int currentPosition = 0;
+            int skipSize = 0;
+
+            foreach (int sliceLenght in input)
+            {
+
+                List<int> subList = new List<int>();
+                for (int i = currentPosition; i < currentPosition + sliceLenght; i++)
+                {
+                    int index = i % list.Count;
+
+                    subList.Add(list[index]);
+                }
+
+                subList.Reverse();
+
+                int sublistIndex = 0;
+                for (int i = currentPosition; i < currentPosition + sliceLenght; i++)
+                {
+                    int index = i % list.Count;
+
+                    list[index] = subList[sublistIndex];
+                    sublistIndex++;
+                }
+
+                currentPosition += sliceLenght + skipSize;
+                currentPosition = currentPosition % list.Count;
+
+                skipSize++;
+            }
+
+            return list[0] * list[1];
         }
     }
 }
