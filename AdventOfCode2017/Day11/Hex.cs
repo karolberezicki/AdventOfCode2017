ea3f945762ff62d4f2d35d6e4527a64dfb42cf50
Karol Berezicki <karolberezicki@gmail.com>
2017-12-11 06:57:27 +0100

    Day1, Part one and two

---
+++ AdventOfCode2017/Day11/Hex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day11
+{
+    public struct Hex
+    {
+        public readonly int Q, R, S;
+
+        public Hex(int q, int r, int s)
+        {
+            Q = q;
+            R = r;
+            S = s;
+        }
+        public static Hex Hex_Subtract(Hex a, Hex b)
+        {
+            return new Hex(a.Q - b.Q, a.R - b.R, a.S - b.S);
+        }
+
+        public static int Hex_Length(Hex hex)
+        {
+            return (Math.Abs(hex.Q) + Math.Abs(hex.R) + Math.Abs(hex.S)) / 2;
+        }
+
+        public static int Hex_Distance(Hex a, Hex b)
+        {
+            return Hex_Length(Hex_Subtract(a, b));
+        }
+    };
+}
--- AdventOfCode2017/Day11/Program11.cs
+++ AdventOfCode2017/Day11/Program11.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day11
 {
@@ -13,6 +11,52 @@ namespace Day11
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+            List<string> input = source.Split(',').ToList();
+
+            Hex start = new Hex();
+            Hex current = new Hex();
+
+            int maxDistanceFromStart = 0;
+
+            foreach (string step in input)
+            {
+                switch (step)
+                {
+                    case "n":
+                        current = new Hex(current.R, current.Q-1, current.S+1);
+                        break;
+
+                    case "s":
+                        current = new Hex(current.R, current.Q+1, current.S-1);
+                        break;
+
+                    case "ne":
+                        current = new Hex(current.R + 1, current.Q-1, current.S);
+                        break;
+
+                    case "se":
+                        current = new Hex(current.R + 1, current.Q, current.S - 1);
+                        break;
+
+                    case "nw":
+                        current = new Hex(current.R - 1, current.Q, current.S+1);
+                        break;
+
+                    case "sw":
+                        current = new Hex(current.R - 1, current.Q+1, current.S);
+                        break;
+                }
+
+                maxDistanceFromStart = Math.Max(maxDistanceFromStart, Hex.Hex_Distance(current, start));
+            }
+
+            int distanceFromStart = Hex.Hex_Distance(current, start);
+
+            Console.WriteLine($"Part one: {distanceFromStart}");
+            Console.WriteLine($"Part two: {maxDistanceFromStart}");
+
+            Console.ReadKey();
+
         }
     }
 }
