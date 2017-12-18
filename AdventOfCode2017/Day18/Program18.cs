b979f827aa43216599ab0eff2f60d89da82b46d7
Karol Berezicki <karolberezicki@gmail.com>
2017-12-18 18:14:50 +0100

    Day18, part one

--- AdventOfCode2017/Day18/Program18.cs
+++ AdventOfCode2017/Day18/Program18.cs
@@ -13,6 +13,103 @@ namespace Day18
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
+            var input = source.Split('\n').Select(c => c.Split(' ').Select(d => d.Trim()).ToList()).ToList();
+
+            Dictionary<string, long> registers = new Dictionary<string, long>();
+
+            foreach (string s in input.Select(c => c[1]))
+            {
+                if (!registers.ContainsKey(s) && !int.TryParse(s, out int c))
+                {
+                    registers.Add(s, 0);
+                }
+            }
+
+            long lastPlayedFrequency = 0;
+
+            lastPlayedFrequency = PartOne(input, registers, lastPlayedFrequency);
+
+        }
+
+        private static long PartOne(List<List<string>> input, Dictionary<string, long> registers, long lastPlayedFrequency)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                var inst = input[i][0];
+                long val;
+                switch (inst)
+                {
+                    case "snd":
+                        lastPlayedFrequency = registers[input[i][1]];
+                        break;
+                    case "set":
+                        if (long.TryParse(input[i][2], out val))
+                        {
+                            registers[input[i][1]] = val;
+                        }
+                        else
+                        {
+                            registers[input[i][1]] = registers[input[i][2]];
+                        }
+                        break;
+                    case "add":
+                        if (long.TryParse(input[i][2], out val))
+                        {
+                            registers[input[i][1]] = registers[input[i][1]] + val;
+                        }
+                        else
+                        {
+                            registers[input[i][1]] = registers[input[i][1]] + registers[input[i][2]];
+                        }
+                        break;
+                    case "mul":
+                        if (long.TryParse(input[i][2], out val))
+                        {
+                            registers[input[i][1]] = registers[input[i][1]] * val;
+                        }
+                        else
+                        {
+                            registers[input[i][1]] = registers[input[i][1]] * registers[input[i][2]];
+                        }
+                        break;
+                    case "mod":
+                        if (long.TryParse(input[i][2], out val))
+                        {
+                            registers[input[i][1]] = registers[input[i][1]] % val;
+                        }
+                        else
+                        {
+                            registers[input[i][1]] = registers[input[i][1]] % registers[input[i][2]];
+                        }
+                        break;
+                    case "rcv":
+                        if (registers[input[i][1]] != 0)
+                        {
+                            registers[input[i][1]] = lastPlayedFrequency;
+                            return lastPlayedFrequency;
+                        }
+                        break;
+                    case "jgz":
+                        if (!long.TryParse(input[i][1], out val))
+                        {
+                            val = registers[input[i][1]];
+                        }
+                        if (val > 0)
+                        {
+                            if (long.TryParse(input[i][2], out val))
+                            {
+                                i = (int)(i + val - 1);
+                            }
+                            else
+                            {
+                                i = (int)(i + registers[input[i][2]] - 1);
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return lastPlayedFrequency;
         }
     }
 }
